8c8b63931778d3bd22f9a8a98bf8701fdbe72e0b
kazukiSR <solomon.rosenfeld@gmail.com>
2022-03-10 20:53:54 -0600

    pen can move and clear ("c" key)

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ screen = Screen()
 
 
 def move_forward():
-    pen.forward(10)
-
+    pen.forward(5)
+def rotate_left():
+    pen.left(5)
+def rotate_right():
+    pen.right(5)
+def move_back():
+    pen.back(5)
+def clear():
+    pen.clear()
+    pen.penup()
+    pen.home()
+    pen.pendown()
 
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkeypress(key="w", fun=move_forward)
+screen.onkeypress(key="a", fun=rotate_left)
+screen.onkeypress(key="d", fun=rotate_right)
+screen.onkeypress(key="s", fun=move_back)
+screen.onkeypress(key="c", fun=clear)
 screen.exitonclick()
